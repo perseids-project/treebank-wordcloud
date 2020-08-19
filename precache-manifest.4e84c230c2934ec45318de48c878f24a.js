b78dcee101953b80d7439c6f5db9b4146341d260
zfletch <zfletch2@gmail.com>
2020-08-18 20:00:41 -0400

    Updates

---
+++ precache-manifest.4e84c230c2934ec45318de48c878f24a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717f96b91b7a988f22165235830522e1",
+    "url": "/treebank-wordcloud/index.html"
+  },
+  {
+    "revision": "ec74a21a5b0f50e80436",
+    "url": "/treebank-wordcloud/static/css/2.0f3a6430.chunk.css"
+  },
+  {
+    "revision": "1578a44c6db41452dc7f",
+    "url": "/treebank-wordcloud/static/css/main.590a9490.chunk.css"
+  },
+  {
+    "revision": "ec74a21a5b0f50e80436",
+    "url": "/treebank-wordcloud/static/js/2.c4ca52d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/treebank-wordcloud/static/js/2.c4ca52d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1578a44c6db41452dc7f",
+    "url": "/treebank-wordcloud/static/js/main.c1af3530.chunk.js"
+  },
+  {
+    "revision": "985cacb51025b9e113f4",
+    "url": "/treebank-wordcloud/static/js/runtime-main.f58ae128.js"
+  }
+]);
