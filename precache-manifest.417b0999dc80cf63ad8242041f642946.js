8a47abb7bd42f626054c70efb8743d337fbd0f44
zfletch <zfletch@users.noreply.github.com>
2020-08-28 16:13:31 +0000

    Deploying to gh-pages from  @ 51e87e26b96ac08afd3d24a1e5bff3796f9e216e 🚀

---
+++ precache-manifest.417b0999dc80cf63ad8242041f642946.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b03cba4da87bafc2ecba4de3b201b697",
+    "url": "/treebank-wordcloud/index.html"
+  },
+  {
+    "revision": "898b749a263dbf6a2fc9",
+    "url": "/treebank-wordcloud/static/css/2.338bb585.chunk.css"
+  },
+  {
+    "revision": "27a847d630687b464cb7",
+    "url": "/treebank-wordcloud/static/css/main.590a9490.chunk.css"
+  },
+  {
+    "revision": "898b749a263dbf6a2fc9",
+    "url": "/treebank-wordcloud/static/js/2.3d6b5d41.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/treebank-wordcloud/static/js/2.3d6b5d41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a847d630687b464cb7",
+    "url": "/treebank-wordcloud/static/js/main.6331c5a4.chunk.js"
+  },
+  {
+    "revision": "985cacb51025b9e113f4",
+    "url": "/treebank-wordcloud/static/js/runtime-main.f58ae128.js"
+  },
+  {
+    "revision": "fcd98c950776a4e4093224e51948125e",
+    "url": "/treebank-wordcloud/static/media/click-treebank.fcd98c95.png"
+  },
+  {
+    "revision": "98123df3a4d30f7fc7c4c2e4bfdb06d1",
+    "url": "/treebank-wordcloud/static/media/copy-link.98123df3.png"
+  },
+  {
+    "revision": "7aa3c197d4d461bca335c69c650fee9e",
+    "url": "/treebank-wordcloud/static/media/create-wordcloud.7aa3c197.png"
+  },
+  {
+    "revision": "1d6c25d580c2733a19db687bad8e1a70",
+    "url": "/treebank-wordcloud/static/media/paste-link.1d6c25d5.png"
+  }
+]);
